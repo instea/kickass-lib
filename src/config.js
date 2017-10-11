eca2936b2fb194bcf039eb93abff244ac9209042
Juraj Matuš <matus.juraj@yandex.com>
2017-10-11 16:17:43 +0200

    Github tokens for both urls

--- src/config.js
+++ src/config.js
@@ -1,8 +1,12 @@
 // @flow
 
 const config = {
+  // For url: https://instea.github.io/kickass-lib/
   GITHUB_CLIENT_ID: '8854c61e8d8a70aa89fc',
   GITHUB_CLIENT_SECRET: 'f70dcfe4d3d44eb25e2b334e51fe09d329caed89',
+  // For url: http://localhost:3000
+  //GITHUB_CLIENT_ID: 'b9470cae4abc802c75d8',
+  //GITHUB_CLIENT_SECRET: '4e9553db79bcff677b95eec1306680098b225bad',
 }
 
 export default config
